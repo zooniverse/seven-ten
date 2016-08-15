4994c96359e30cae0b2f855943a576aca0fed605
Michael Parrish <michael@zooniverse.org>
2016-08-15 14:07:32 -0500

    Expand ApplicationSerializer to support sorting, filtering, etc.

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,2 +1,32 @@
 class ApplicationSerializer < ActiveModel::Serializer
+  class << self
+    attr_accessor :filterable_attributes, :sortable_attributes, :default_sort
+  end
+
+  attribute :id
+
+  def self.filterable_by(*attrs)
+    attrs.each do |attr|
+      self.filterable_attributes << attr.to_sym
+      self.filterable_attributes.uniq!
+    end
+  end
+
+  def self.sortable_by(*attrs)
+    attrs.each do |attr|
+      self.sortable_attributes << attr.to_sym
+      self.sortable_attributes.uniq!
+    end
+  end
+
+  def self.default_sort_by(attr)
+    self.default_sort = attr.to_sym
+  end
+
+  def self.inherited(klass)
+    klass.filterable_attributes = filterable_attributes&.dup || []
+    klass.sortable_attributes = sortable_attributes&.dup || []
+    klass.default_sort = default_sort&.dup || ''
+    super
+  end
 end
--- spec/serializers/application_serializer_spec.rb
+++ spec/serializers/application_serializer_spec.rb
@@ -1,3 +1,36 @@
 RSpec.describe ApplicationSerializer, type: :serializer do
+  let(:model) do
+    Class.new(ActiveModelSerializers::Model) do
+      attr_accessor :id, :attr1, :attr2
+    end
+  end
 
+  let(:serializer) do
+    Class.new(ApplicationSerializer) do
+      attributes :id, :attr1, :attr2
+      filterable_by :id, :attr1
+      sortable_by :attr1, :attr2
+      default_sort_by :id
+    end
+  end
+
+  describe '.attributes' do
+    subject{ ApplicationSerializer.new model.new(id: 1, attr1: 'nope') }
+    its(:as_json){ is_expected.to eql id: 1 }
+  end
+
+  describe '.filterable_attributes' do
+    subject{ serializer.filterable_attributes }
+    it{ is_expected.to match_array [:id, :attr1] }
+  end
+
+  describe '.sortable_attributes' do
+    subject{ serializer.sortable_attributes }
+    it{ is_expected.to match_array [:attr1, :attr2] }
+  end
+
+  describe '.default_sort' do
+    subject{ serializer.default_sort }
+    it{ is_expected.to eql :id }
+  end
 end
