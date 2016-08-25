6391429c9a004e3bdd4827052a0d45e87a777736
Michael Parrish <michael@zooniverse.org>
2016-08-25 11:14:45 -0500

    Add ApplicationRecord#with_retry

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def with_retry(error:, attempts:)
+    try_count = 0
+    begin
+      yield
+    rescue error => e
+      try_count += 1
+      try_count < attempts ? retry : raise(e)
+    end
+  end
 end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -1,3 +1,45 @@
 RSpec.describe ApplicationRecord, type: :model do
+  describe '#with_retry' do
+    let(:record){ create :split }
+    let(:block){ double }
+    subject do
+      record.with_retry(error: RuntimeError, attempts: 2){ block.call }
+    end
 
+    def expect_call
+      expect(block).to receive(:call).once.ordered
+    end
+
+    context 'with an intermittent exception' do
+      before(:each) do
+        expect_call.and_raise 'error'
+        expect_call.and_return 'worked'
+      end
+
+      it 'should succeed' do
+        expect(subject).to eql 'worked'
+      end
+    end
+
+    context 'with a persistent exception' do
+      before(:each) do
+        expect_call.and_raise 'error'
+        expect_call.and_raise 'error'
+      end
+
+      it 'should re-raise the error' do
+        expect{ subject }.to raise_error 'error'
+      end
+    end
+
+    context 'with an unspecified exception' do
+      before(:each) do
+        expect_call.and_raise TypeError.new 'oops'
+      end
+
+      it 'should not retry' do
+        expect{ subject }.to raise_error TypeError
+      end
+    end
+  end
 end
