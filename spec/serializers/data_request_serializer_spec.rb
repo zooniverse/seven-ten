872621ff9c86ec547b5367b6f56040e607b13e83
Michael Parrish <michael@zooniverse.org>
2016-10-25 11:32:51 -0500

    Relax time-based expectations

--- spec/serializers/data_request_serializer_spec.rb
+++ spec/serializers/data_request_serializer_spec.rb
@@ -27,8 +27,8 @@ RSpec.describe DataRequestSerializer, type: :serializer do
     its([:split_id]){ is_expected.to eql data_request.split_id }
     its([:state]){ is_expected.to eql data_request.state }
     its([:url]){ is_expected.to eql data_request.url }
-    its([:created_at]){ is_expected.to eql data_request.created_at }
-    its([:updated_at]){ is_expected.to eql data_request.updated_at }
+    its([:created_at]){ is_expected.to be_within(1.second).of data_request.created_at }
+    its([:updated_at]){ is_expected.to be_within(1.second).of data_request.updated_at }
   end
 
   describe '#links' do
