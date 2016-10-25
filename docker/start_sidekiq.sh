84e800d7085f35d8a99accd6492c774a98616b79
Michael Parrish <michael@zooniverse.org>
2016-10-25 13:47:12 -0500

    Add sidekiq start script

---
+++ docker/start_sidekiq.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$RAILS_ENV" == "staging" || "$RAILS_ENV" == "production" ]]; then
+  aws s3 sync s3://zooniverse-code/production_configs/seven_ten_$RAILS_ENV config
+fi
+
+bundle install --without test development
+rake db:migrate
+sidekiq
