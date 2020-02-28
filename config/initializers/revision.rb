588a3c97eedf052b2e61e91af83c66145b485094
Zach Wolfenbarger <zach.wolfenbarger@gmail.com>
2020-02-28 15:54:21 -0600

    Change revision file name

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,4 +1,4 @@
-revision_file = Rails.root.join 'config/revision.txt'
+revision_file = Rails.root.join 'config/commit_id.txt'
 
 if File.exists?(revision_file)
   Rails.application.revision = File.read(revision_file).chomp
